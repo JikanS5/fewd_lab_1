cd55f2b5cc0e9d05710caa71f94520563dfe7eb4
JikanS5 <charli.deluca@yahoo.fr>
2020-03-12 16:13:46 +0000

    finis

---
+++ lab1.js
@@ -0,0 +1,22 @@
+var firstName = 'Charli';
+var favoriteColor = 'grey';
+var favouriteFood = 'carbonara';
+var city= 'Paris';
+
+//print my first name, my favorite color and food and my city
+console.log(firstName);
+console.log(favoriteColor);
+console.log(favoriteColor);
+console.log(city);
+
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+console.log(phrase + ', '+ one +', '+two +', '+ three +', ' + four +', '+ five +', '+six +', '+ seven+' ' + endingPhrase);
